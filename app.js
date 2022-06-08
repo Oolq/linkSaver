61cafbedb0abb98c6aeca274be2e26de16db0470
jamez <christianheramis02@gmail.com>
2022-06-08 18:00:49 +0800

    adding and initializing js doc

---
+++ app.js
@@ -0,0 +1,20 @@
+const linkCategory = document.querySelector("#linkCategory");
+
+let linkCategories = [];
+
+linkCategory.addEventListener('keydown', function (event) {
+
+    if (event.keyCode === 188) {
+        event.preventDefault();
+
+        linkCategories.push(linkCategory.value);
+
+        linkCategory.value = '';
+
+        displayLinkCategories();
+    }
+})
+
+function displayLinkCategories() {
+    console.log("display link");
+}
